0e240083df227096dbe130d837ae8c046ba9a627
ferparra <ferparra83@gmail.com>
2010-03-16 01:16:54 -0300

    Modificar vista de empleador para mostrar siempre opción de crear trabajo. Modificado modelo de worker para validar presencia de last_name

--- app/models/worker.rb
+++ app/models/worker.rb
@@ -2,7 +2,7 @@ class Worker < ActiveRecord::Base
   has_many :bids
   has_many :employers, :through => :bids
   #belongs_to :user
-  validates_presence_of :first_name, :second_name
+  validates_presence_of :first_name, :last_name
   
   def full_name
     first_name + " " + last_name
