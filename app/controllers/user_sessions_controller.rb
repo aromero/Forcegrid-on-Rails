83aac35e976027f28cc6e2ad8da34f1e0d3cb8e7
ferparra <ferparra83@gmail.com>
2010-03-16 22:37:35 -0300

    Agrego require en user_sessions_controller por errores en templating

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,3 +1,5 @@
+require 'authlogic'
+
 class UserSessionsController < ApplicationController
   before_filter :require_no_user, :only => [:new, :create]
   before_filter :require_user, :only => :destroy
