3d9334c441df475907c8eedb6d0901a65d77c586
ferparra <ferparra83@gmail.com>
2010-03-18 22:41:34 -0300

    Agregado plugin de gmail

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,6 +2,6 @@ class Job < ActiveRecord::Base
   belongs_to :employer
   has_one :bid
   
-  validates_presence_of :title, :description, :start_time, :end_time, :budget
+  validates_presence_of :title, :description, :start_time, :end_time, :budget, :employer_id
 
 end
