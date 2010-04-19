0ea04b04de1b8befd2b80e382a5e521bc149ff88
Fernando Parra <ferparra83@gmail.com>
2010-04-19 16:20:05 -0400

    Removí del index el datafile y agregue seeds de Skills

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,29 @@
 # Archivo de datos seed
 
 # Categorías
-cat = []
+cat = Array.new
 cat[0] = { :name => 'Servicios de Infraestructura', :description => 'Servicios de Infraestructura de IT' }
-cat[1] = { :name => 'Desarrollo Web', :description => 'Desarrollo de aplicaciones y sitios web' }
+cat[1] = { :name => 'Desarrollo', :description => 'Desarrollo de aplicaciones y sitios web' }
 categories = Category.create(cat)
 
 # Subcategorías
-sub_cat = []
-sub_cat[0] = { :name => 'Backups', :description => 'Gestión de Backups', :category_id => categories[0].id }
+sub_cat = Array.new
+sub_cat[0] = { :name => 'ITIL', :description => 'Gestión de Backups', :category_id => categories[0].id }
 sub_cat[1] = { :name => 'Virtualización', :description => 'Gestión de máquinas virtuales', :category_id => categories[0].id }
-sub_cat[2] = { :name => 'Rails', :description => 'Ruby on Rails', :category_id => categories[1].id }
-sub_cat[3] = { :name => 'Python', :description => 'Python y Django', :category_id => categories[1].id }
+sub_cat[2] = { :name => 'Web', :description => 'Ruby on Rails', :category_id => categories[1].id }
+sub_cat[3] = { :name => 'Desktop', :description => 'Python y Django', :category_id => categories[1].id }
 subcategories = Category.create(sub_cat)
+
+# Skills
+skill = Array.new
+skill[0] = { :name => 'Configuration Management', :category_id => subcategories[0].id }
+skill[1] = { :name => 'Incident Management', :category_id => subcategories[0].id }
+skill[2] = { :name => 'Provisioning', :category_id => subcategories[1].id }
+skill[3] = { :name => 'Ruby on Rails', :category_id => subcategories[2].id }
+skill[4] = { :name => 'Django', :category_id => subcategories[2].id }
+skill[5] = { :name => 'Swing', :category_id => subcategories[2].id }
+skill[6] = { :name => 'Windows forms', :category_id => subcategories[2].id }
+skills = Skill.create(skill)
 
 # Usuario administrador
 admin = User.create(:email => 'fernando.parra@itparc.com', :password => 'itparc', :admin => true)
@@ -21,6 +32,7 @@ admin.activate!
 # Worker
 user1 = User.new(:email => 'ferparra83@gmail.com', :password => 'itparc')
 user1.owner = Worker.new(:first_name => 'Fernando', :last_name => 'Parra', :address1 => 'Raffo 2976')
+user1.owner.skills << skills[3]
 user1.save
 user1.activate!
 
