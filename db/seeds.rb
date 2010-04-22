31c902ecfd74d51f956e500b9d8ca6dcddae0966
Fernando Parra <ferparra83@gmail.com>
2010-04-22 18:21:05 -0400

    Category seeds arreglados

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,26 @@
 # Archivo de datos seed
 
 # Categorías
-categories = Category.create(
-    :name => 'Servicios de Infraestructura', :description => 'Servicios de Infraestructura de IT', 
-    :name => 'Desarrollo', :description => 'Desarrollo de aplicaciones y sitios web')
+categories = Category.create([
+    { :name => 'Servicios de Infraestructura', :description => 'Servicios de Infraestructura de IT' }, 
+    { :name => 'Desarrollo', :description => 'Desarrollo de aplicaciones y sitios web' }])
 
 # Subcategorías
-subcategories = Category.create(
-    :name => 'ITIL', :description => 'Gestión de Backups', :category_id => categories[0].id, 
-    :name => 'Virtualización', :description => 'Gestión de máquinas virtuales', :category_id => categories[0].id, 
-    :name => 'Web', :description => 'Desarrollo Web', :category_id => categories[1].id, 
-    :name => 'Desktop', :description => 'Fat Clients', :category_id => categories[1].id)
+subcategories = Category.create([
+    { :name => 'ITIL', :description => 'Gestión de Backups', :category_id => categories[0].id }, 
+    { :name => 'Virtualización', :description => 'Gestión de máquinas virtuales', :category_id => categories[0].id }, 
+    { :name => 'Web', :description => 'Desarrollo Web', :category_id => categories[1].id }, 
+    { :name => 'Desktop', :description => 'Fat Clients', :category_id => categories[1].id }])
 
 # Skills
-skills = Skill.create(
-    :name => 'Configuration Management', :category_id => subcategories[0].id, 
-    :name => 'Incident Management', :category_id => subcategories[0].id, 
-    :name => 'Provisioning', :category_id => subcategories[1].id, 
-    :name => 'Ruby on Rails', :category_id => subcategories[2].id, 
-    :name => 'Django', :category_id => subcategories[2].id, 
-    :name => 'Swing', :category_id => subcategories[3].id, 
-    :name => 'Windows forms', :category_id => subcategories[3].id)
+skills = Skill.create([
+    { :name => 'Configuration Management', :category_id => subcategories[0].id }, 
+    { :name => 'Incident Management', :category_id => subcategories[0].id }, 
+    { :name => 'Provisioning', :category_id => subcategories[1].id }, 
+    { :name => 'Ruby on Rails', :category_id => subcategories[2].id }, 
+    { :name => 'Django', :category_id => subcategories[2].id }, 
+    { :name => 'Swing', :category_id => subcategories[3].id }, 
+    { :name => 'Windows forms', :category_id => subcategories[3].id }])
 
 # Usuario administrador
 admin = User.create(:email => 'fernando.parra@itparc.com', :password => 'itparc', :admin => true)
@@ -42,5 +42,5 @@ user2.activate!
 #Sample Job
 job = Job.create(:title => 'Web Site', :description => 'Necesito un website',
       :start_time => 3.days.from_now, :end_time => 15.days.from_now,
-      :employer_id => user2.id, :category_id => categories[1].id,
+      :employer_id => user2.id, :category_id => Category.first.id,
       :project_start => 17.days.from_now, :project_finish => 22.days.from_now)
