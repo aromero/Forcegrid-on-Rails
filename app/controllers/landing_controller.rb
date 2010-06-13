aff46fefbd8c04cd20d0b4c2d09c52b62375e11c
Fernando Parra <ferparra83@gmail.com>
2010-06-13 19:39:43 -0400

    Layout de landing más contenido estático de muestra

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,12 +1,10 @@
 class LandingController < ApplicationController
+  layout 'landing'
   def index
     if current_user && current_user.employer?
       @jobs = current_user.owner.jobs
     else
       @jobs = Job.current
     end
-  end
-  
-  def register
   end
 end
