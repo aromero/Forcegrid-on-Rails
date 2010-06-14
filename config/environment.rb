afb35556f9604e7e469d015b84ec0776972b48e0
Fernando Parra <ferparra83@gmail.com>
2010-06-13 20:46:51 -0400

    Prueba de setear encoding en UTF8

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,7 @@
 
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
+Encoding.default_external = Encoding::UTF_8 if RUBY_VERSION > "1.9"
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
