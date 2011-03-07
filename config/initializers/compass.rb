b6c669daf8881c32b2d2255190060f60906d7a8b
Fernando Parra <ferparra83@gmail.com>
2011-03-07 17:43:32 -0300

    Modificación a inicio de compass p/heroku según http://devcenter.heroku.com/articles/using-compass

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,3 +1,8 @@
-require 'compass'
-require 'compass/app_integration/rails'
+require 'fileutils'
+FileUtils.mkdir_p(Rails.root.join("tmp", "stylesheets"))
+
 Compass::AppIntegration::Rails.initialize!
+
+Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Rack::Static',
+    :urls => ['/stylesheets'],
+    :root => "#{Rails.root}/tmp")
